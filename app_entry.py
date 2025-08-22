81adff1e32e481be731edc49d7444416c751f30b
Tomas Flecchia <flecchia.tomas@gmail.com>
2025-08-22 21:37:41 +0200

    Changed root from /todo-project to /

--- app_entry.py
+++ app_entry.py
@@ -1,9 +1,13 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from main import app  # tu main.py
+# from werkzeug.middleware.dispatcher import DispatcherMiddleware
+# from main import app  # tu main.py
+#
+# application = DispatcherMiddleware(
+#     None,
+#     {
+#         '/todo-project': app
+#     }
+# )
 
-application = DispatcherMiddleware(
-    None,
-    {
-        '/todo-project': app
-    }
-)
+from main import app
+
+application = app
